e264421efa722f225d97f4385e2781f5484b34cb
João Carlos Rodrigues Jr <jc.rodrigues1997@gmail.com>
2022-10-18 19:54:59 -0300

    adding code scaffolding

--- fbsdcme/__init__.py
+++ fbsdcme/__init__.py
@@ -1,1 +1,27 @@
-__version__ = '0.1.0'
+import os, sys, subprocess
+
+__version__ = "0.1.0"
+
+
+def _bin_path(program):
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), "bin", program)
+
+
+def _run(program):
+    sys.exit(subprocess.call([_bin_path(program), *sys.argv[1:]]))
+
+
+def stater_space_scheme20():
+    _run("stater_space_scheme20")
+
+
+def prop_mx_scheme20():
+    _run("prop_mx_scheme20")
+
+
+def ss_prob_dist_scheme20():
+    _run("ss_prob_dist_scheme20")
+
+
+def mxexpo():
+    _run("mxexpo")
