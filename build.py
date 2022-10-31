d88f7e952c454ae66112941e25bf857ebe92bea9
Hendrik Dumith Louzada <hendriklouzada@gmail.com>
2022-10-30 23:48:02 -0300

    add build instructions and configure poetry

---
+++ build.py
@@ -0,0 +1,52 @@
+"""Build script."""
+
+import shutil
+from distutils import log as distutils_log
+from pathlib import Path
+from typing import Any, Dict
+
+import skbuild
+import skbuild.constants
+
+__all__ = ("build",)
+
+
+def build(setup_kwargs: Dict[str, Any]) -> None:
+    """Build C-extensions."""
+    skbuild.setup(**setup_kwargs, script_args=["build_ext"])
+
+    src_dir = Path(skbuild.constants.CMAKE_INSTALL_DIR()) / "bin"
+    dest_dir = Path("bin")
+
+    # Delete C-extensions copied in previous runs, just in case.
+    remove_files(dest_dir, "*")
+
+    # Copy built C-extensions back to the project.
+    copy_files(src_dir, dest_dir, "*")
+
+
+def remove_files(target_dir: Path, pattern: str) -> None:
+    """Delete files matched with a glob pattern in a directory tree."""
+    for path in target_dir.glob(pattern):
+        if path.is_dir():
+            shutil.rmtree(path)
+        else:
+            path.unlink()
+        distutils_log.info(f"removed {path}")
+
+
+def copy_files(src_dir: Path, dest_dir: Path, pattern: str) -> None:
+    """Copy files matched with a glob pattern in a directory tree to another."""
+    for src in src_dir.glob(pattern):
+        dest = dest_dir / src.relative_to(src_dir)
+        if src.is_dir():
+            # NOTE: inefficient if subdirectories also match to the pattern.
+            copy_files(src, dest, "*")
+        else:
+            dest.parent.mkdir(parents=True, exist_ok=True)
+            shutil.copy2(src, dest)
+            distutils_log.info(f"copied {src} to {dest}")
+
+
+if __name__ == "__main__":
+    build({})
