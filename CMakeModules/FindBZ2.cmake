1bfe929b0ae4377d329abcd8b6703bd44223abe3
Hendrik Dumith Louzada <hendriklouzada@gmail.com>
2022-10-31 22:43:50 -0300

    use static or dynamic libsbml and add missing find cmake modules

---
+++ CMakeModules/FindBZ2.cmake
@@ -0,0 +1,63 @@
+include(LibFindMacros)
+include(GNUInstallDirs)
+
+find_path(BZ2_INCLUDE_DIR
+  NAMES bzlib.h bzip2/bzlib.h
+  PATHS ${BZ2_PKGCONF_INCLUDE_DIRS}
+  ${CMAKE_INSTALL_FULL_INCLUDEDIR}
+  $ENV{HOME}/usr/include
+  /usr/local/include
+)
+
+if (BZ2_INCLUDE_DIR)
+    file(STRINGS "${BZ2_INCLUDE_DIR}/bzlib.h" BZLIB_H REGEX "bzip2/libbzip2 version [0-9]+\\.[^ ]+ of [0-9]+ ")
+    string(REGEX REPLACE ".* bzip2/libbzip2 version ([0-9]+\\.[^ ]+) of [0-9]+ .*" "\\1" BZ2_VERSION "${BZLIB_H}")
+endif ()
+
+find_library(BZ2_LIBRARY
+  NAMES bzip2 bz2
+  PATHS ${LIBZ_PKGCONF_LIBRARY_DIRS}
+  ${CMAKE_INSTALL_FULL_LIBDIR}
+  $ENV{HOME}/usr/lib
+  /usr/local/lib
+)
+
+include(CheckLibraryExists)
+
+if (BZ2_LIBRARY)
+    # make sure that we have a valid bzip2 library
+    file(TO_CMAKE_PATH "${BZ2_LIBRARY}" BZ2_CMAKE_PATH)
+    CHECK_LIBRARY_EXISTS("${BZ2_CMAKE_PATH}" "BZ2_bzCompressInit" "" BZ2_FOUND_SYMBOL)
+    if(NOT BZ2_FOUND_SYMBOL)
+        # this is odd, but on windows this check always fails! must be a
+        # bug in the current cmake version so for now only issue this
+        # warning on linux
+        if(UNIX)
+            message(WARNING
+"The chosen bz2 library does not appear to be valid because it is
+missing some required symbols. Please check that ${BZ2_LIBRARY}
+is the bzip2 library. For details about the error, please see
+${LIBSBML_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeError.log")
+        endif()
+    endif()
+    if(NOT EXISTS "${BZ2_INCLUDE_DIR}/bzlib.h")
+        message(FATAL_ERROR
+"The include directory specified for the bz2 library does not
+appear to be valid.  It should contain the file bzlib.h, but
+it does not.")
+    endif()
+endif()
+
+# create an bz2 target to link against
+if(NOT TARGET BZ2::BZ2)
+  add_library(BZ2::BZ2 UNKNOWN IMPORTED)
+  set_target_properties(BZ2::BZ2 PROPERTIES
+    IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+    IMPORTED_LOCATION "${BZ2_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${BZ2_INCLUDE_DIR}"
+    )
+endif()
+
+set(BZ2_PROCESS_INCLUDES BZ2_INCLUDE_DIR)
+set(BZ2_PROCESS_LIBS BZ2_LIBRARY)
+libfind_process(BZ2)
---
+++ CMakeModules/FindZLIB.cmake
@@ -0,0 +1,89 @@
+# Copyright (C) 2022 by Pedro Mendes, Rector and Visitors of the 
+# University of Virginia, University of Heidelberg, and University 
+# of Connecticut School of Medicine. 
+# All rights reserved. 
+
+# - Try to find the Zlib XML parsing library 
+
+# Once done this will define
+#
+#  ZLIB_FOUND       - system has Zlib
+#  ZLIB_LIBRARIES   - Link these to use Zlib
+#  ZLIB_INCLUDE_DIR - Include directory for using Zlib
+#  ZLIB_DEFINITIONS - Compiler switches required for using Zlib
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+include(LibFindMacros)
+
+string(TOUPPER ${PROJECT_NAME} _UPPER_PROJECT_NAME)
+set(_PROJECT_DEPENDENCY_DIR ${_UPPER_PROJECT_NAME}_DEPENDENCY_DIR)
+
+# Check if we have cached results in case the last round was successful.
+if (NOT (ZLIB_INCLUDE_DIR AND ZLIB_LIBRARY) OR NOT ZLIB_FOUND)
+
+    set(ZLIB_LDFLAGS)
+	
+    find_path(ZLIB_INCLUDE_DIR zlib.h zlib/zlib.h
+	    PATHS $ENV{ZLIB_DIR}/include
+	          $ENV{ZLIB_DIR}
+              ${${_PROJECT_DEPENDENCY_DIR}}
+              ${${_PROJECT_DEPENDENCY_DIR}}/include
+              ~/Library/Frameworks
+              /Library/Frameworks
+              /sw/include        # Fink
+              /opt/local/include # MacPorts
+              /opt/csw/include   # Blastwave
+              /opt/include
+              /usr/freeware/include
+    )
+
+    find_library(ZLIB_LIBRARY 
+	    NAMES z zlib 
+	    PATHS ${CMAKE_INSTALL_FULL_LIBDIR}
+              $ENV{ZLIB_DIR}/lib
+	          $ENV{ZLIB_DIR}/lib-dbg
+	          $ENV{ZLIB_DIR}
+              $ENV{HOME}/usr/lib
+              /usr/local/lib
+              ${${_PROJECT_DEPENDENCY_DIR}}
+              ${${_PROJECT_DEPENDENCY_DIR}}/${CMAKE_INSTALL_LIBDIR}
+              ${CONAN_LIB_DIRS_LIBSBML}
+              $ENV{HOME}/Library/Frameworks
+              /Library/Frameworks
+              /sw/lib        # Fink
+              /opt/local/lib # MacPorts
+              /opt/csw/lib   # Blastwave
+              /opt/lib
+              /usr/freeware/lib64
+    )
+
+    if (NOT WIN32)
+        find_package(PkgConfig)
+        pkg_check_modules(PC_ZLIB QUIET zlib)
+
+        message(VERBOSE "${PC_ZLIB_STATIC_LDFLAGS}")
+
+        if (PC_ZLIB_FOUND)
+            set(ZLIB_DEFINITIONS ${PC_ZLIB_CFLAGS_OTHER})
+            set(ZLIB_VERSION ${PC_ZLIB_VERSION} CACHE STRING "Zlib Version found" )
+        endif (PC_ZLIB_FOUND)
+    endif (NOT WIN32)
+    
+    mark_as_advanced(ZLIB_INCLUDE_DIR ZLIB_LIBRARY)
+
+endif () # Check for cached values
+
+# create an zlib target to link against
+if(NOT TARGET ZLIB::ZLIB)
+  add_library(ZLIB::ZLIB UNKNOWN IMPORTED)
+  set_target_properties(ZLIB::ZLIB PROPERTIES
+    IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+    IMPORTED_LOCATION "${ZLIB_LIBRARY}"
+    INTERFACE_INCLUDE_DIRECTORIES "${ZLIB_INCLUDE_DIR}")
+endif()
+
+set(ZLIB_PROCESS_INCLUDES ZLIB_INCLUDE_DIR)
+set(ZLIB_PROCESS_LIBS ZLIB_LIBRARY)
+libfind_process(ZLIB)
