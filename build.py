d95d4cae809750cd189870f41006640116873576
Hendrik Dumith Louzada <hendriklouzada@gmail.com>
2022-11-06 17:11:01 -0300

    fix poetry generating wrong wheel target

--- build.py
+++ build.py
@@ -1,9 +1,11 @@
 """Build script."""
 
 import os
+import setuptools
 import shutil
 import subprocess
 from distutils import log as distutils_log
+from distutils.util import get_platform
 from pathlib import Path
 from typing import Any, Dict
 
@@ -17,17 +19,17 @@ PROJECT_SOURCE = Path(__file__).resolve().parent
 PROJECT_BUILD_SOURCE = PROJECT_SOURCE / "build"
 
 
-def configure_cmake() -> None:
+def configure_cmake(*args) -> None:
     """Configure CMake."""
     environment_variable = dict(list(os.environ.items()))
-    cmd = [CMAKE_DEFAULT_EXECUTABLE, "-S", str(PROJECT_SOURCE), "-B", "build"]
+    cmd = [CMAKE_DEFAULT_EXECUTABLE, "-S", str(PROJECT_SOURCE), "-B", "build", *args]
     subprocess.call(cmd, cwd=PROJECT_SOURCE, env=environment_variable)
 
 
-def run_make() -> None:
+def run_make(*args) -> None:
     """Run CMake build."""
     env = dict(list(os.environ.items()))
-    cmd = [CMAKE_DEFAULT_EXECUTABLE, "--build", "build"]
+    cmd = [CMAKE_DEFAULT_EXECUTABLE, "--build", "build", *args]
     subprocess.call(cmd, cwd=PROJECT_SOURCE, env=env)
 
 
@@ -47,6 +49,18 @@ def build(setup_kwargs: Dict[str, Any]) -> None:
     # Copy built C-extensions back to the project.
     copy_files(src_dir, dest_dir, "*")
 
+    # Run targeted build separately as poetry does not support target builds yet
+    try:
+        setuptools.setup(
+            **setup_kwargs, 
+            script_args = ['bdist_wheel'],
+            options = { 
+                'bdist_wheel': { 'plat_name': os.getenv('PP_PYTHON_TARGET', get_platform()) },
+                'egg_info': { 'egg_base': './dist/' }
+            }
+        )
+    except BaseException as e:
+        distutils_log.error(f"Failed to create targeted wheel: {e}")
 
 def remove_files(target_dir: Path, pattern: str) -> None:
     """Delete files matched with a glob pattern in a directory tree."""
@@ -69,7 +83,3 @@ def copy_files(src_dir: Path, dest_dir: Path, pattern: str) -> None:
             dest.parent.mkdir(parents=True, exist_ok=True)
             shutil.copy2(src, dest)
             distutils_log.info(f"copied {src} to {dest}")
-
-
-if __name__ == "__main__":
-    build({})
